853412953009759527c6e12cd94fc1dcd3f05c27
matias <maperazzo@itba.edu.ar>
2016-10-21 15:51:35 -0300

    new stylesheet and js, minor changes on header

---
+++ js/mijs.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+		$('select').material_select();
+      $('.autocomplete').autocomplete({
+        data: {
+           "Apple": null,
+           "Microsoft": null,
+           "Google": null
+        }
+     });
+      $('.datepicker').pickadate({
+        selectMonths: true, 
+        selectYears: 15
+     });
+
+      var roundTrip = document.getElementById("roundTrip");
+      roundTrip.checked = true;
+
+      roundTrip.onclick = function() {
+         document.getElementById("return").style.display = 'block';  
+      }
+
+      var oneWay = document.getElementById("oneWay");
+      oneWay.onclick = function() {
+         document.getElementById("return").style.display = 'none';  
+      }
+
+      
+	});
