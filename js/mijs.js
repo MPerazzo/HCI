07e2c0d194bd2424bdfb762d36b880019a8d6d59
matias <maperazzo@itba.edu.ar>
2016-10-22 05:34:23 -0300

    comment box text aligns left, date input changes and others

--- js/mijs.js
+++ js/mijs.js
@@ -9,8 +9,10 @@ $(document).ready(function(){
      });
       $('.datepicker').pickadate({
         min:new Date('today'),
+        max:new Date(2020,11,31), /* materialize datepicker max limit is 31/12/2020 based on how it works */
         selectMonths: true, 
         selectYears: 5,
+        format: 'dd/mm/yyyy',
         onClose: function() {
           $(document.activeElement).blur();
         }
